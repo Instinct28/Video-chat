21b0192a0530e08da5218a9192febc893de04e61
Yash Kumar Bandhiye <69250462+Instinct28@users.noreply.github.com>
2024-04-01 20:07:17 +0530

    Add files via upload

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,5 +1,5 @@
 export const APP_NAME = 'Videet';
 export const repoName = 'https://github.com/Instinct28/Video-chata';
-export const APP_URL = 'https://video-chat-1-y9cn.onrender.com';
+export const APP_URL = 'https://videet.netlify.app/';
 export const BACKEND_URL = 'https://video-chat-4rw0.onrender.com';
 // export const BACKEND_URL='http://localhost:5000';
